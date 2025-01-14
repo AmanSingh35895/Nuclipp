6f1bf7446cd5eb746573852490b4e9d08b0de290
AmanSingh35895 <smohan35895@gmail.com>
2025-01-14 20:39:25 +0530

    added <Link> in the header

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Header = () => {
   return (
@@ -10,15 +11,15 @@ export const Header = () => {
       />
       <div className='flex w-[65%] justify-between font-semibold font-inter tracking-wide items-center'>
         <div className='flex w-[80%] justify-between'>
-          <p>Home</p>
-          <p>Our Story</p>
-          <p>Services</p>
-          <p>Results & Success</p>
-          <p>Pricing</p>
-          <p>More &gt;</p>
+          <Link to ='/'>Home</Link>
+          <Link to = '/our_story'>Our Story</Link>
+          <Link to = '/services'>Services</Link>
+          <Link to = '/'>Results & Success</Link>
+          <Link>Pricing</Link>
+          <Link>More &gt;</Link>
         </div>
 
-        <button className='flex bg-[#9FD8DF] rounded-[5px] py-[12px] px-[18px]'>Get Started</button>
+        <button className='flex bg-[#9FD8DF] rounded-[5px] py-[12px] px-[18px]'>Book a call</button>
       </div>
     </div>
   )
