6db9a83cb52d3c7c830677666fc52a1946983c7c
Shubham Mishra <m.shubham0723@gmail.com>
2025-01-15 01:22:42 +0530

    updated styling

--- src/pages/Home/components/Hero.jsx
+++ src/pages/Home/components/Hero.jsx
@@ -3,13 +3,17 @@ import videoBanner from '../../../assets/images/Video.png'
 
 const Hero = () => {
   return (
-    <div className='flex flex-col items-center gap-y-4 justify-center h-screen  shadow-xl shadow-[#0000000F] rounded-[15px]'>
-      <p className='font-inter font-semibold text-[3rem]'>Turn your YouTube dream into reality</p>
-      <p className='text-[#475569] font-normal w-[25%] text-center'>From Monetized Channels to Organic Growth, NUCLIPP Builds Your Success</p>
-      <button className='border-[1.5px] border-[#FF7171] py-[14px] px-[20px] rounded-[5px] font-inter font-semibold hover:bg-[#FF7171] hover:text-[#F8FAFC] duration-500'>Start Your Journey Now</button>
-      <div className='w-full'>
-        <img src={videoBanner}/>
-      </div>
+    <div className='flex flex-col items-center gap-y-4 justify-center min-h-screen pb-16  shadow-xl shadow-[#0000000F] rounded-[15px] mt-28'>
+      <div className='flex flex-col justify-center items-center space-y-32'>
+        <div className='flex flex-col justify-center items-center space-y-8'>
+          <p className='font-inter font-semibold text-6xl tracking-tighter'>Turn your YouTube dream into reality</p>
+          <p className='text-[#475569] text-lg max-w-96 text-center'>From Monetized Channels to Organic Growth, NUCLIPP Builds Your Success</p>
+          <button className='border-[1.5px] border-[#FF7171] py-[14px] px-[20px] rounded-[5px] font-inter font-semibold hover:bg-[#FF7171] hover:text-[#F8FAFC] duration-500'>Start Your Journey Now</button>
+        </div>
+        <div className='w-auto max-w-[60%]'>
+          <img src={videoBanner} className="w-full h-auto" alt="Video promotion" />
+        </div>
+     </div>
     </div>
   );
 };
--- src/pages/Home/components/ProblemStatement.jsx
+++ src/pages/Home/components/ProblemStatement.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
+import BG from '../../../assets/images/BG.png';
 
 const ProblemStatement = () => {
   return (
-    <div className='w-full flex justify-center h-screen shadow-xl shadow-[#0000000F] rounded-[15px] font-inter'>
-      <div className='w-[50%] h-full flex flex-col pl-[15%] pt-[5rem]'>
-        <p className='font-semibold text-[3rem]'>Tired of Struggling?</p>
-        <p className='font-normal text-[2rem] py-2'>We’ve Got You Covered</p>
-        <p className='font-normal text-justify text-[1rem] py-2 w-[61%] tracking-wider text-[#0F172A]'>
-          Creators worldwide struggle with Monetizing and  Managing their YouTube channels effectively.
-        </p>
-        <p className='font-normal text-justify text-[1rem] py-2 w-[61%] tracking-wider text-[#0F172A]'>
-          Many give up, facing algorithm challenges, monetization policies, and the relentless demand for quality content. NUCLIPP is here to change that narrative.
-        </p>
+    <div className='w-full flex justify-center space-x-32 min-h-screen shadow-xl shadow-[#0000000F]  font-inter px-32 py-16'>
+      <div className='w-[60%] h-full flex flex-col space-y-16 border-red-300 border-2 py-8 '>
+        <div className='text-left  border-red-300 border-2'>
+          <p className='font-semibold text-6xl'>Tired of Struggling?</p>
+          <p className='font-normal text-[2rem] py-2'>We’ve Got You Covered</p>
+        </div>
+        <div className='border-red-300 border-2'>
+          <p className='font-normal text-justify text-[1rem] py-2  tracking-wider text-[#0F172A]'>
+            Creators worldwide struggle with Monetizing and  Managing their YouTube channels effectively.
+          </p>
+          <p className='font-normal text-justify text-[1rem] py-2 tracking-wider text-[#0F172A]'>
+            Many give up, facing algorithm challenges, monetization policies, and the relentless demand for quality content. NUCLIPP is here to change that narrative.
+          </p>
+        </div>
       </div>
-      <div className='w-[50%]'>
+      <div className='w-[40%]'>
+           <img src={BG} className="w-full h-auto" alt="Video promotion" />
       </div>
     </div>
   );
