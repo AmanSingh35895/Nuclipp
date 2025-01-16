c1abbd159d0cb505e281ae7f9aa2e066c159d2ce
rishirishav0912 <rishirishav912@gmail.com>
2025-01-16 10:41:37 +0530

    added-arrow-image

--- src/pages/Pricing/components/Hero.jsx
+++ src/pages/Pricing/components/Hero.jsx
@@ -8,17 +8,17 @@ const Hero = ({plan,setPlan}) => {
   }
 
   return (
-    <div className='flex flex-col text-justify items-center gap-y-1 justify-start pt-40 h-[59vh] font-dm-sans'>
+    <div className='flex flex-col text-justify items-center gap-y-1 justify-start pt-40 h-[66vh] font-dm-sans'>
       <p className='font-inter font-bold text-[3rem]'>
         Powerful features for
       </p>
       <p className='text-[#FF7171] text-[3rem] font-bold text-center'>
         powerful creators
       </p>
-      <p className='text-[#191D23] text-[1.5rem] font-normal w-[25%] text-center'>
+      <p className='text-[#191D23] text-[1.5rem] pt-2 font-normal w-[25%] text-center'>
         Choose a plan that’s right for you
       </p>
-      <div className='flex text-[#191D23] gap-x-4 pt-10 font-normal text-[1rem]'>
+      <div className='flex text-[#191D23] gap-x-4 pt-[4rem] font-normal text-[1rem] w-fit pl-40'>
         <span>Pay Monthly </span>
         <Switch
           onChange={handleChange} 
@@ -29,6 +29,14 @@ const Hero = ({plan,setPlan}) => {
           checked={plan === 'monthly' ? false : true}
         />
         <span>Pay Quaterly</span>
+        <span className='text-[#FF7171] flex'>
+          <img 
+            src='assets\images\arrow.png'
+            alt='arrow'
+            className='h-[4rem] w-[5rem] '
+          />
+          <span className='pt-8 font-medium'><span>Save up to 20%</span></span>
+        </span>
       </div>
     </div>
   );
